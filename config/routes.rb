fdfba6b25b874d901cf83143877dc6c79b0a887f
Brendan Ritchie <brendan@sandglaz.com>
2015-01-04 15:40:17 -0500

    Implement signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'about' => 'static_pages#about'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
